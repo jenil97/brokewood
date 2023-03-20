a6a147872170c6a0ae5a2a11b53d4a7be54729c5
Jenil Patel <jenilweb10@gmail.com>
2023-03-20 17:16:59 +0530

    feat : Features

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import Hero from "./components/Hero";
+import Features from "./components/Features";
 
 function App() {
   return (
     <div className="w-full max-w-[1440px] mx-auto bg-white">
       <Hero />
+      <Features />
     </div>
   );
 }
